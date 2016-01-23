5a96a03bed0bdbff92d23762745b2d8f6b69c811
hudecsamuel <hudecsamuel@gmail.com>
2016-01-23 19:36:19 +0100

    new frontend

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+var socket = io();
+
+socket.on('connect', function(){
+  console.log('Connected to socket.io server');
+});
+
+socket.on('message', function(message) {
+  console.log('New message:');
+  console.log(message.text);
+});
+
+socket.emit('message', {
+  text: 'This is message'
+});
