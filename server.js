91f5b5cfc0e749aee765b9bedc7f614c70c4de9d
hudecsamuel <hudecsamuel@gmail.com>
2016-02-23 20:41:35 +0100

    Add bootstrap and styles

--- server.js
+++ server.js
@@ -9,6 +9,34 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+//sends current users to defined socket
+function sendCurrentUsers(socket) {
+  var info = clientInfo[socket.id];
+  var users = [];
+  console.log('info may be undefined');
+
+  if(typeof info === 'undefined') {
+    return;
+  }
+  
+  console.log(info);
+
+  Object.keys(clientInfo).forEach(function(socketId) {
+    var userInfo = clientInfo[socketId];
+
+    if (info.room === userInfo.room) {
+      users.push(userInfo.name);
+    }
+  });
+  console.log(users);
+
+  socket.emit('message', {
+    name: 'System',
+    text: 'Current users: ' + users.join(', '),
+    timestamp: moment().valueOf()
+  });
+};
+
 io.on('connection', function(socket){
   console.log('User connected via socket.io');
 
@@ -37,8 +65,14 @@ io.on('connection', function(socket){
   socket.on('message', function(message){
     console.log('Message recieved ' + message.text);
 
-    message.timestamp = moment().valueOf();
-    io.to(clientInfo[socket.id].room).emit('message', message);
+    if(message.text === '@currentUsers') {
+      console.log('current users');
+      sendCurrentUsers(socket);
+    } else {
+      message.timestamp = moment().valueOf();
+      io.to(clientInfo[socket.id].room).emit('message', message);
+    }
+
   });
 
   //timestamp property - Javascript timestamp (miliseconds)
